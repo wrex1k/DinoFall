929ff61ecaa2260ab0c31445278019b47d6c8007
wrex1k <156697493+wrex1k@users.noreply.github.com>
2025-01-23 13:41:36 +0100

    basic window

--- game.py
+++ game.py
@@ -5,20 +5,24 @@ class Game:
     def __init__(self):
         pygame.init()   
 
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("dino fall")
 
     def run(self):
         running = True
+        self.clock.tick(60)
         
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            self.screen.fill((0, 0, 0))
+            self.screen.fill('blue')
 
             pygame.display.update()
 
-    pygame.quit()
-    sys.exit()
+        pygame.quit()
+        sys.exit()
+
+Game().run()
