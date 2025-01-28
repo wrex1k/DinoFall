47c51623d22955ed73f1c312c19559bd20ece86b
wrex1k <156697493+wrex1k@users.noreply.github.com>
2025-01-28 12:57:08 +0100

    added spawn logic for platforms

--- game.py
+++ game.py
@@ -1,28 +1,49 @@
-import pygame, sys
+import pygame, sys, random
 from settings import screen_width, screen_height
 from player import Player
+from platform import Platform
 
 class Game:
     def __init__(self):
         pygame.init()   
-        pygame.display.set_caption("dino fall")
 
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         self.clock = pygame.time.Clock()
-        
-        self.player = Player(self.screen, screen_width // 2 - 24, 0)
+        self.delta_time = 0
+
+        self.player = Player(self.screen, 0, 0)
+        self.platforms = pygame.sprite.Group()
+
+        self.font = pygame.font.Font('assets/fonts/Minecraft.ttf', 30)
+        self.text = self.font.render(f"test", True, (0, 0, 0))
+
+        self.create_platforms(6)
+
+    def create_platforms (self, num):
+        for id in range(1, num):
+            self.platforms.add(Platform(self.screen, self.platforms))
+
     def run(self):
         running = True
         while running:
-            self.clock.tick(60)
-            self.screen.fill((0, 120, 220))
-
+            delta_time = self.clock.tick(60) / 1000.0
+            pygame.display.set_caption(str(round(self.clock.get_fps(), 2)))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key in (pygame.K_y, pygame.K_x, pygame.K_c, pygame.K_v):
+                        colors = {pygame.K_y: 'yellow', pygame.K_x: 'red', pygame.K_c: 'blue', pygame.K_v: 'green'}
+                        self.player.change_dino(colors[event.key])
 
-            self.player.update()
-            self.player.draw()
+            self.screen.fill((255, 255, 255))
+           
+            self.player.update(delta_time, self.platforms)
+            self.platforms.update(delta_time)
+            self.player.draw() 
+            self.platforms.draw(self.screen)
+
+            self.screen.blit(self.text, (0, 0))
 
             pygame.display.update()
 
--- platform.py
+++ platform.py
@@ -1,26 +1,47 @@
-import pygame
+import pygame, random
 from settings import screen_width, screen_height
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, screen, color, type, x, y):
-        super().__init__()
-        self.screen = screen
-        self.image = pygame.image.load(f'assets/platforms/{color}/{type}.png').convert_alpha()
-        self.scale = [3, 3] if type == 'small' else [6, 3] if type == 'medium' else None
-        self.rect = pygame.Rect(x, y, 48*self.scale[0], 16.0*self.scale[1])
-        self.speed = 100
+    zones = [140, 280, 420, 560, 700]
 
-    def update(self, delta_time):
+    def __init__(self, surface, group):
+        super().__init__()
+        self.surface = surface
+        self.color = random.choice(['green'])
+        self.type = 'small' if random.randint(1, 3) % 2 == 1 else 'medium'
+        self.image = pygame.image.load(f'assets/platforms/{self.color}/{self.type}.png').convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, (3, 3))
+        self.x = random.randint(0, screen_width - self.image.get_width())
+        self.y = self.set_y()
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.speed = 80
+        self.group = group
+
+    def get_info(self):
+        print(f"Color: {self.color}, Type: {self.type}, X: {self.x}, Y: {self.y}")
+
+    def set_y(self):
+        if len(Platform.zones) > 0:
+            num = random.choice(Platform.zones)
+            self.zones.remove(num)
+        else:
+            num = 700
+        return num
+
+    def create_platform(self):
+        platform = Platform(self.surface, self.group)
+        platform.get_info()
+        self.group.add(platform)
+    
+    def update(self, delta_time):        
         self.rect.y -= self.speed * delta_time
 
         if self.rect.bottom <= 0:
-            self.rect.top = screen_height
+            self.kill()
+            self.create_platform()
 
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(self.image, self.rect)
 
-    def draw_hitbox(self):
-        pygame.draw.rect(self.screen, (255, 0, 0), self.rect)
-
-    def draw(self):
-        self.draw_hitbox()
-        scaled_image = pygame.transform.scale(self.image, (48*self.scale[0], 16*self.scale[1]))
-        self.screen.blit(scaled_image, self.rect) 
