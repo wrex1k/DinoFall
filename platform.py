9a98fd3e494d46482e8974247e2c4e671ad4fc74
wrex1k <156697493+wrex1k@users.noreply.github.com>
2025-02-01 17:05:50 +0100

    update platform

--- platform.py
+++ platform.py
@@ -1,47 +1,81 @@
 import pygame, random
 from settings import screen_width, screen_height
+from coin import Coin
 
 class Platform(pygame.sprite.Sprite):
-    zones = [140, 280, 420, 560, 700]
+    zones = [140, 280, 420, 560, 700, 840] # default y positions for the platforms
+    colors = ['blue', 'green', 'red', 'yellow']
 
-    def __init__(self, surface, group):
+    def __init__(self, id, screen, player):
         super().__init__()
-        self.surface = surface
-        self.color = random.choice(['green'])
-        self.type = 'small' if random.randint(1, 3) % 2 == 1 else 'medium'
-        self.image = pygame.image.load(f'assets/platforms/{self.color}/{self.type}.png').convert_alpha()
-        self.image = pygame.transform.scale_by(self.image, (3, 3))
-        self.x = random.randint(0, screen_width - self.image.get_width())
-        self.y = self.set_y()
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
-        self.mask = pygame.mask.from_surface(self.image)
-        self.speed = 80
-        self.group = group
+        self.player = player
+        self.id = id
+        self.screen = screen
+        self.speed = 100
+        self.set_atributes()
 
+    # info about platform
     def get_info(self):
-        print(f"Color: {self.color}, Type: {self.type}, X: {self.x}, Y: {self.y}")
+        print(f"id: {self.id}, Color: {self.color}, Palette: {Platform.colors}, Speed: {self.speed}, Type: {self.type}, X: {self.x}, Y: {self.y}")
+    
+    # set atributes of the platform / respawn
+    def set_atributes(self):
+        self.color = self.random_color()
+        self.type = random.choices(['small', 'medium'], weights=[1, 0.4])[0]
+        self.image_path = f'assets/platforms/{self.color}/{self.type}.png'
+        self.image = pygame.image.load(self.image_path).convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, 3)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.x = self.set_pos('x')
+        self.y = self.set_pos('y')
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.coin = self.spawn_coin()
 
-    def set_y(self):
-        if len(Platform.zones) > 0:
-            num = random.choice(Platform.zones)
-            self.zones.remove(num)
+    # spawn coin on the platform
+    def spawn_coin(self):
+        num = random.randint(0, 100)
+        if num > 50 and self.type == 'medium':
+            coin = Coin(self.screen, self.rect.topleft, self.rect.topright, self.speed)
+            return coin
+
+    # set platform color
+    def random_color(self):
+        if len(Platform.colors) > 0:
+            color = random.choice(Platform.colors)
+            Platform.colors.remove(color)
         else:
-            num = 700
+            Platform.colors = ['blue', 'green', 'red', 'yellow']
+            color = random.choice(Platform.colors)
+        return color
+
+    # set pos of the platform 
+    def set_pos(self, axis):
+        if axis == 'x':
+            num = random.randint(0, screen_width - self.image.get_width() - 30)
+        elif axis == 'y':
+            if len(Platform.zones) > 0:
+                num = random.choice(Platform.zones)
+                Platform.zones.remove(num)
+            else:
+                num = screen_width
         return num
 
-    def create_platform(self):
-        platform = Platform(self.surface, self.group)
-        platform.get_info()
-        self.group.add(platform)
-    
-    def update(self, delta_time):        
+    # update
+    def update(self, delta_time):
+        if self.coin:
+            if self.coin.rect.colliderect(self.player.rect) and not self.coin.collect:
+                self.player.coins += 1
+                self.coin.collect = True
+            self.coin.update(delta_time)
+            self.coin.draw()
+        if self.rect.bottom <= 0:
+            self.set_atributes()
+
         self.rect.y -= self.speed * delta_time
 
-        if self.rect.bottom <= 0:
-            self.kill()
-            self.create_platform()
 
-    def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
-        surface.blit(self.image, self.rect)
+
+    def draw(self):
+    # draw
+        self.screen.blit(self.image, self.rect)
 
