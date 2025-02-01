1bd9d2944c2cbc1a2fd6214a78086acefeedc28b
wrex1k <156697493+wrex1k@users.noreply.github.com>
2025-02-01 17:06:16 +0100

    update player

--- player.py
+++ player.py
@@ -8,15 +8,18 @@ class Player(pygame.sprite.Sprite):
         self.scale = 3
         self.rect = pygame.Rect(x, y, 24*self.scale, 24*self.scale-10)
         self.direction = 'right'
-        self.speed = 200
-        self.velocity_y = 3
-        self.gravity = 2
-        self.hp = 3
+        self.speed = 220
+        self.velocity_y = 0
+        self.gravity = 1.5
+        self.score = 1
+        self.coins = 0
 
         self.dinos = ['blue', 'red', 'yellow', 'green']
         self.current_color = 'blue'
-        self.load_animations(self.current_color)
 
+        self.all_animations = {color: self.load_animations(color) for color in self.dinos}
+
+        self.animations = self.all_animations[self.current_color]
         self.current_animation = self.animations['idle_right']
         self.animation_frame = 0
 
@@ -24,21 +27,23 @@ class Player(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def get_info(self):
-        print(f"x: {self.rect.x} | y: {self.rect.y} | G: {self.gravity} | velocity_y: {self.velocity_y} | on_platform: {self.on_platform}")
+        print(f"x: {self.rect.x} | y: {self.rect.y} | G: {self.gravity} | velocity_y: {self.velocity_y} | on_platform: {self.on_platform} | Correct color: {self.correct_color} | speed: {self.speed}")
 
+    # load all animations for each direction at once by color
     def load_animations(self, color):
         path = f'assets/sheets/DinoSprites - {color}.png'
         spritesheet = pygame.image.load(path).convert_alpha()
 
-        self.animations = {
-            'idle_right': self.load_animation(spritesheet, 0, 3),
-            'idle_left': self.load_animation(spritesheet, 0, 3, flip=True),
-            'walk_right': self.load_animation(spritesheet, 4, 8),
-            'walk_left': self.load_animation(spritesheet, 4, 8, flip=True),
-            'dead': self.load_animation(spritesheet, 15, 17),
+        return {
+            'idle_right': self.load_frames(spritesheet, 0, 3),
+            'idle_left': self.load_frames(spritesheet, 0, 3, flip=True),
+            'walk_right': self.load_frames(spritesheet, 4, 8),
+            'walk_left': self.load_frames(spritesheet, 4, 8, flip=True),
+            'dead': self.load_frames(spritesheet, 15, 17),
         }
 
-    def load_animation(self, spritesheet, start, stop, flip=False):
+    # load frames from spritesheet in right size than scale it, if animation left, flip frame
+    def load_frames(self, spritesheet, start, stop, flip=False):
         frames = []
         for i in range(start, stop):
             frame = spritesheet.subsurface(pygame.Rect(i * 24, 0, 24, 24))
@@ -48,18 +53,23 @@ class Player(pygame.sprite.Sprite):
             frames.append(frame)
         return frames
 
+    # change color, animation of dino, set default animation
     def change_dino(self, color):
-        if color != self.current_color:
+        if color in self.all_animations:
             self.current_color = color
-            self.load_animations(color)
-            self.current_animation = self.animations['idle_right']
+            self.animations = self.all_animations[color]  
+            self.current_animation = self.all_animations[color]['idle_right']
 
     def update(self, delta_time, platforms):
-        keys = pygame.key.get_pressed()
-        moving = False
         self.on_platform = False
         old_x = self.rect.x  
+        moving = False
 
+        # update score
+        self.score += 1
+
+        # input handler
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= self.speed * delta_time
             self.direction = 'left'
@@ -69,24 +79,35 @@ class Player(pygame.sprite.Sprite):
             self.direction = 'right'
             moving = True
 
-        if not self.on_platform:
-            self.velocity_y += self.gravity * delta_time
-            self.rect.y += self.velocity_y
-            
+        # collision detection by mask and apply gravity 
         for platform in platforms:
             if pygame.sprite.collide_mask(self, platform):
+                
+                # speed handler
+                self.speed = 220 if platform.color == self.current_color else 100
+                
+                # collision detection
                 if self.rect.bottom <= platform.rect.top + 10:
-                    self.rect.bottom = platform.rect.top
+                    self.rect.bottom = platform.rect.top 
                     self.velocity_y = 0
                     self.on_platform = True
-                    self.rect.y -= platform.speed * delta_time
                     break
                 else:
+                    self.rect.y -= self.speed * delta_time
                     self.rect.x = old_x
-            else:
-                self.velocity_y = 3
 
-        if self.rect.top >= self.screen.get_height(): 
+            if not self.on_platform:
+                self.velocity_y += self.gravity * delta_time
+                
+        # apply gravity
+        self.rect.y += self.velocity_y
+
+        # velocity limit 5
+        if not self.on_platform:
+            self.velocity_y = min(self.velocity_y, 5)
+
+        # reset y position
+        if self.rect.bottom >= screen_height:
             self.rect.bottom = 0
 
         if moving:
@@ -100,8 +121,7 @@ class Player(pygame.sprite.Sprite):
 
         self.image = self.current_animation[int(self.animation_frame)]
 
-        self.get_info()
+        #self.get_info()
 
     def draw(self):
-        #pygame.draw.rect(self.screen, (255, 0, 0), self.rect)
         self.screen.blit(self.image, self.rect)
